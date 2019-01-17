4049f03cf6c41be9b05612ea9213fcb9da4b19ab
Siddhant Misra <msiddhant22@gmail.com>
2019-01-17 15:27:45 -0800

    A ReactJs Webapp.
    A ReactJS webapp that tells you the weather once you  type in which country and it tells you the required weather conditions.

---
+++ README.md
@@ -0,0 +1,3 @@
+﻿This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
+
+Shoutout to Hamza Mirza: https://github.com/hamza-mirza/react-weather-app/
---
+++ src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="title-container__title">Weather Finder</h1>
+		<h3 className="title-container__subtitle">Humidity, Conditions, Temparature and obviously Weather</h3>
+	</div>
+);
+
+export default Titles;
